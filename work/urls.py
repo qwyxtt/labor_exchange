bd8ed7896ef8e804d9d07762273ae745dd8b51a7
unknown <savkom513@gmail.com git config --global user.name>
2023-02-27 18:30:20 +0300

    'new'

--- work/urls.py
+++ work/urls.py
@@ -4,5 +4,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('account/', TemplateView.as_view(template_name='authentications/account.html'), name='account'),
-    path('index/', index)
+    path('index/', index, name='index')
 ]
