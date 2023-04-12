0c2972078c9bfb911d289dec81a15506941097bd
unknown <savkom513@gmail.com git config --global user.name>
2023-04-12 18:25:57 +0300

    id

--- work/urls.py
+++ work/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('create_task/', create_task, name='create_task'),
     path('history_of_task/', task_history_employee, name='history_of_task'),
     path('work/', work, name='work'),
-    path('task/<int:task_id>/', accept_task, name='accept_task')
+    path('task/<int:task_id>/', accept_task, name='accept_task'),
+    path('get_task/', TemplateView.as_view(template_name='work/get_work.html'), name='get_work'),
+    path('task/<int:task_id>/go-to-task', go_to_task, name='go_to_task')
 ]
